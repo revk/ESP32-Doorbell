57f2c717c5087d1141435da85df1c64ff8028070
RevK <github@k.gg>
2023-12-15 18:05:35 +0000

    checkpoint

--- main/Doorbell.c
+++ main/Doorbell.c
@@ -560,6 +560,7 @@ app_main ()
       register_get_uri ("/active", web_active);
       revk_web_settings_add (webserver);
    }
+   sleep(1);
    {
     const char *e = gfx_init (cs: port_mask (gfxcs), sck: port_mask (gfxsck), mosi: port_mask (gfxmosi), dc: port_mask (gfxdc), rst: port_mask (gfxrst), busy: port_mask (gfxbusy), ena: port_mask (gfxena), flip: gfxflip, direct: 1, invert:gfxinvert);
       if (e)
